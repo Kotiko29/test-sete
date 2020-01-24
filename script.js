5cab7cb9ee1cef3b4b794e10f582850f2979fd1d
Kotiko29 <kotiko29@yandex.ru>
2020-01-24 17:02:07 +0500

    cycle For, Do While

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -15,11 +15,45 @@ for (let i = 0; i < 2; i++) {
 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
 		b = prompt("Во сколько обойдется?", '');
 
-		if ((typeof(a)) === `string` && (typeof(a)) !=null && (typeof(b)) != null && a != `` && b != `` && a.langth < 50) {
-			console.log(done);
+		if ((typeof(a)) === `string` && (typeof(a)) !=null && (typeof(b)) != null && a != `` && b != `` && a.length < 50) {
+			console.log('done');
 			appData.expenses[a] = b;
-		} //else  надо вернуться обратно в цикл
+		} else {
+			alert('Еще раз!');
+		}//else  надо вернуться обратно в цикл
 }
+
+// let i =0;
+// while (i < 2) {
+// 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 			b = prompt("Во сколько обойдется?", '');
+
+// 		if ((typeof(a)) === `string` && (typeof(a)) !=null && (typeof(b)) != null && a != `` && b != `` && a.length < 50) {
+// 			console.log('done');
+// 			appData.expenses[a] = b;
+// 			i++;
+// 		} else {
+// 			alert('Еще раз!');
+// 		}//else  надо вернуться обратно в цикл
+// }
+
+// let i = 0;
+// do {
+// 	i++;
+// 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 				b = prompt("Во сколько обойдется?", '');
+	
+// 			if ((typeof(a)) === `string` && (typeof(a)) !=null && (typeof(b)) != null && a != `` && b != `` && a.length < 50) {
+// 				console.log('done');
+// 				appData.expenses[a] = b;
+// 				i++;
+// 			} else {
+// 				alert('Еще раз!');
+// 			}//else  надо вернуться обратно в цикл
+// } while (i<=2);
+	
+
+
 
 appData.moneyPerDay = appData.budget / 30;
 
